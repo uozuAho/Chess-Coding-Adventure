f2c57fb83ab43bbf80a2d5de120cab565f398cf6
me <me@me.com>
2025-07-26 14:06:11 +1000

    add search test

---
+++ Chess-Coding-Adventure.Test/SearcherTests.cs
@@ -0,0 +1,36 @@
+using Chess.Core;
+using Shouldly;
+
+namespace Chess_Coding_Adventure.Test;
+
+public class SearcherTests
+{
+    [Fact]
+    public void ChoosesAMove()
+    {
+        var board = Board.CreateBoard();
+        var search = new Searcher(board);
+
+        Move? move = null;
+        search.OnSearchComplete += m => move = m;
+
+        Task.Run(() => search.StartSearch());
+        Task.Delay(10).Wait();
+        search.EndSearch();
+        Task.Run(() =>
+        {
+            while (move == null)
+            {
+                Thread.Sleep(1);
+            }
+        }).Wait();
+
+        move.ShouldNotBeNull();
+        search.searchDiagnostics.numPositionsEvaluated.ShouldBeGreaterThan(10);
+        search.searchDiagnostics.isBook.ShouldBeFalse(
+            "searcher doesn't use book openings, bot does");
+
+        // uncomment this to see some debug output
+        // search.debugInfo.ShouldBeEmpty();
+    }
+}
